3a6602c7a41ba7e1988f90e00ae3afdfdc7f3d8a
ENKNOT <256907366+enknot96@users.noreply.github.com>
2026-09-04 21:45:21 +0900

    各種ファイルの修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         background: resolve(__dirname, 'src/background.ts'),
         content: resolve(__dirname, 'src/content/index.ts'),
+        popup: resolve(__dirname, 'popup.html'),
       },
       output: {
         entryFileNames: '[name].js',
